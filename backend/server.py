de8bfb00657e3ce02f3ffc002d37920f345eeed4
kanachi123 <shrekkanachyan1234@gmail.com>
2025-05-05 21:58:21 +0400

    python changes

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,30 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+import json
+import sympy as sp
 
 server = Flask(__name__)
+
+JSON_FILE = 'data.json'
+
+@server.route('/',methods=['POST'])
+
+
+def get_data():
+    data = request.get_json()
+    if(not data or 'formula' not in data or 'segment' not in data):
+        return jsonify({'error':'Invalid Input'}),400
+    
+def valid_formula(formula):
+    try:
+        x = sp.Symbol('x')
+        expr = sp.sympify(formula)
+        return True
+    except sp.SympifyError:
+        return False
+
+
+def valid_segment():
+    
+
 
 
