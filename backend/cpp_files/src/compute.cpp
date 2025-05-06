fe0cdd7e0f8f31124d3c4fd59b42c2228a7bc534
kanachi123 <shrekkanachyan1234@gmail.com>
2025-05-07 00:24:36 +0400

    mUUUUUUtex

--- backend/cpp_files/src/compute.cpp
+++ backend/cpp_files/src/compute.cpp
@@ -5,8 +5,35 @@
 #include <mutex>
 #include <string>
 #include <muParser.h>
+#include <vector>
 
 using json = nlohmann::json;
+
+
+int maxThreads = std::thread::hardware_concurrency();
+std::mutex mtx;
+double h = 0.001;
+
+struct Point {
+    double x, y,dx,dy,ddx,ddy;
+};
+
+double finite_diff_method(const mu::string_type& exp,double x){
+    
+    std::vector<Point> points;
+
+    mu::Parser parser;
+    parser.SetExpr(exp);
+    parser.DefineVar(mu::string_type(L"x"),&x);
+
+    return 0.0;
+}
+
+void compute(){
+
+}
+
+
 
 int main(){
 
