d0ebf84db4971c5644bfbc07a8a2ded5a8c17d29
kanachi123 <shrekkanachyan1234@gmail.com>
2025-05-08 02:29:53 +0400

    point 1.1

--- backend/cpp_files/src/compute.cpp
+++ backend/cpp_files/src/compute.cpp
@@ -13,23 +13,49 @@ using json = nlohmann::json;
 int maxThreads = std::thread::hardware_concurrency();
 std::mutex mtx;
 double h = 0.001;
+std::vector<Point> points;
 
 struct Point {
-    double x, y,dx,dy,ddx,ddy;
+    double x, y,dy_dx,d2y_dx2;
+    Point(double x, double y) : x(0), y(0),dy_dx(0.0),d2y_dx2(0.0) {}
 };
 
-double finite_diff_method(const mu::string_type& exp,double x){
-    
-    std::vector<Point> points;
+void compute(const mu::string_type& exp,const json& data){
 
     mu::Parser parser;
     parser.SetExpr(exp);
+
+    double x = data["lower"],end = data["upper"] + 1;
+    double y {0},i = 0;
+
+
+    while (x < end){
+        
+        parser.DefineVar(mu::string_type(L"x"),&x);
+        points[i].x = x;
+        points[i].y = parser.Eval();
+        x++;i++;
+    }
+}
+
+void finite_diff_method(const mu::string_type& exp,double x,int i){
+    
+    mu::Parser parser;
+    parser.SetExpr(exp);  
+    x +=h;  
     parser.DefineVar(mu::string_type(L"x"),&x);
 
-    return 0.0;
+    points[i].dy_dx = (parser.Eval() - points[i].y) / h;
+    
+    if(i>0)
+    {
+        points[i].d2y_dx2 = (points[i].dy_dx - points[i-1].dy_dx) / h;
+    }
+
 }
 
 void compute(){
+
 
 }
 
